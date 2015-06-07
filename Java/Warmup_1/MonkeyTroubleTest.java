499ee4824dbe36568937f1f20e1713c23d80baec
Ryan Coon <rybread354@aol.com>
2015-06-06 22:12:23 -0500

    Adding warmup 1 problems

---
+++ Java/Warmup_1/MonkeyTroubleTest.java
@@ -0,0 +1,30 @@
+package Warmup_1;
+
+/*
+ * We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if
+ * each is smiling. We are in trouble if they are both smiling or if neither of
+ * 
+ * monkeyTrouble(true, true) = true
+ * monkeyTrouble(false, false) = true
+ * monkeyTrouble(true, false) = false
+ */
+
+public class MonkeyTroubleTest {
+
+	public static void main(String[] args) {
+		MonkeyTroubleTest test = new MonkeyTroubleTest();
+		System.out.println(test.monkeyTrouble(true, true));
+		System.out.println(test.monkeyTrouble(false, false));
+		System.out.println(test.monkeyTrouble(true, false));
+	}
+	
+	public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+		  if (aSmile && bSmile) {
+		      return true;
+		  }
+		  if (!aSmile && !bSmile) {
+		      return true;
+		  }
+		  return false;
+	}
+}
---
+++ Java/Warmup_1/SumDouble.java
@@ -0,0 +1,30 @@
+package Warmup_1;
+
+/*
+ * Given two int values, return their sum. Unless the two values are the same,
+ * then return double their sum.
+ * 
+ * sumDouble(1, 2) = 3
+ * sumDouble(3, 2) = 5
+ * sumDouble(2, 2) = 8
+ */
+public class SumDouble {
+
+	public static void main(String[] args) {
+		SumDouble test = new SumDouble();
+		System.out.println(test.sumDouble(1, 2));
+		System.out.println(test.sumDouble(3, 2));
+		System.out.println(test.sumDouble(2, 2));
+	}
+	
+	public int sumDouble(int a, int b) {
+		  int sum = a + b;
+		  
+		  // Double it if a and b are the same
+		  if (a == b) {
+		      sum = sum * 2;
+		  }
+		  
+		  return sum;
+	}
+}
