90f1cd9f9d3e49bd56dd712b76fc1ad9441c621f
Ryan Coon <rybread354@aol.com>
2015-06-06 22:18:01 -0500

    Added warmup_2 problem

---
+++ Java/Warmup_2/StringTimes.java
@@ -0,0 +1,30 @@
+package Warmup_2;
+
+/*
+ * Given a string and a non-negative int n, return a larger string that is n
+ * copies of the original string.
+ * 
+ * stringTimes("Hi", 2) = "HiHi"
+ * stringTimes("Hi", 3) = "HiHiHi"
+ * stringTimes("Hi", 1) = "Hi"
+ */
+
+public class StringTimes {
+
+	public static void main(String[] args) {
+        StringTimes test = new StringTimes();
+        System.out.println(test.stringTimes("Hi", 2));
+        System.out.println(test.stringTimes("Hi", 3));
+        System.out.println(test.stringTimes("Hi", 1));
+	}
+	
+	public String stringTimes(String str, int n) {
+		  String result = "";
+		  
+		  for (int i = 0; i < n; i++) {
+		      result += str;
+		  }
+		  
+		  return result;
+	}
+}
