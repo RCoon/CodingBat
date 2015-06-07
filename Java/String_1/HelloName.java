78ff8d1e691d7be77db707cb8d0b33b29215eb33
Ryan Coon <rybread354@aol.com>
2015-06-07 16:01:08 -0500

    Add HelloName

---
+++ Java/String_1/HelloName.java
@@ -0,0 +1,25 @@
+package String_1;
+
+/*
+ * Given a string name, e.g. "Bob", return a greeting of the form
+ * "Hello Bob!".
+ * 
+ * helloName("Bob") = "Hello Bob!"
+ * helloName("Alice") = "Hello Alice!"
+ * helloName("X") = "Hello X!"
+ */
+
+public class HelloName {
+
+	public static void main(String[] args) {
+		HelloName test = new HelloName();
+		System.out.println(test.helloName("Bob"));
+		System.out.println(test.helloName("Alice"));
+		System.out.println(test.helloName("X"));
+	}
+	
+	public String helloName(String name) {
+		  return "Hello " + name + "!";
+	}
+
+}
