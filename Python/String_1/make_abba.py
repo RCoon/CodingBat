74ee54faba6413f6e0cf41a5671642368e5963d7
Ryan Coon <rybread354@aol.com>
2015-06-09 11:16:53 -0500

    Added new problems

---
+++ Python/String_1/make_abba.py
@@ -0,0 +1,13 @@
+# Given two strings, a and b, return the result of putting them together in
+# the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+
+# make_abba('Hi', 'Bye') → 'HiByeByeHi'
+# make_abba('Yo', 'Alice') → 'YoAliceAliceYo'
+# make_abba('What', 'Up') → 'WhatUpUpWhat'
+
+def make_abba(a, b):
+  return a + b + b + a
+
+print(make_abba('Hi', 'Bye'))
+print(make_abba('Yo', 'Alice'))
+print(make_abba('What', 'Up'))
