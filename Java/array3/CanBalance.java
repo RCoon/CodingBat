c686638bbab81204b66f14baf404914409767a8e
Ryan Coon <rybread354@aol.com>
2015-06-10 17:22:41 -0500

    Added new problems

---
+++ Java/array3/CanBalance.java
@@ -0,0 +1,38 @@
+package array3;
+
+/*
+ * Given a non-empty array, return true if there is a place to split the array
+ * so that the sum of the numbers on one side is equal to the sum of the 
+ * numbers on the other side.
+ * 
+ * canBalance({1, 1, 1, 2, 1}) -> true
+ * canBalance({2, 1, 1, 2, 1}) -> false
+ * canBalance({10, 10}) -> true
+ */
+
+public class CanBalance {
+	
+	public static void main(String[] args) {
+		CanBalance test = new CanBalance();
+		System.out.println(test.canBalance(new int[] {1, 1, 1, 2, 1}));
+		System.out.println(test.canBalance(new int[] {2, 1, 1, 2, 1}));
+		System.out.println(test.canBalance(new int[] {10, 10}));
+	}
+
+	public boolean canBalance(int[] nums) {
+		  int total = 0;
+		  int revTotal = 0;
+		  
+		  if (nums.length == 1) return false;
+		  
+		  for (int i = 0; i < nums.length; i++) {
+		      total += nums[i];
+		      for (int j = nums.length - 1; j > 0; j--) {
+		          revTotal += nums[j];
+		          if (j == i+1 && total == revTotal) return true;
+		      }
+		      revTotal = 0;
+		  }
+		  return false;
+	}
+}
---
+++ Python/String_1/make_tags.py
@@ -0,0 +1,15 @@
+# The web is built with HTML strings like "<i>Yay</i>" which draws Yay as
+# italic text. In this example, the "i" tag makes <i> and </i> which surround
+# the word "Yay". Given tag and word strings, create the HTML string with tags
+# around the word, e.g. "<i>Yay</i>". 
+
+# make_tags('i', 'Yay') → '<i>Yay</i>'
+# make_tags('i', 'Hello') → '<i>Hello</i>'
+# make_tags('cite', 'Yay') → '<cite>Yay</cite>'
+
+def make_tags(tag, word):
+  return "<{0}>{1}</{2}>".format(tag, word, tag)
+
+print(make_tags('i', 'Yay'))
+print(make_tags('i', 'Hello'))
+print(make_tags('cite', 'Yay'))
