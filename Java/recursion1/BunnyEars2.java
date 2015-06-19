8d5a513e2a3c0a25bcc9bfddb8931648f7be6e4f
Ryan Coon <rybread354@aol.com>
2015-06-18 21:21:39 -0500

    Added new problems

--- Java/recursion1/BunnyEars2.java
+++ Java/recursion1/BunnyEars2.java
@@ -2,8 +2,8 @@ package recursion1;
 
 /* We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies
  * (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say have
- * 3 ears, because they each have a raised foot. Recursively return the number of
- * "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+ * 3 ears, because they each have a raised foot. Recursively return the number
+ * of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
  *
  * bunnyEars2(0) -> 0
  * bunnyEars2(1) -> 2
