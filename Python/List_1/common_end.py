67ef3ab51a8980e49d1220d29cfac5ca841cf250
Ryan Coon <RCoon@users.noreply.github.com>
2015-06-29 23:22:26 -0500

    Update common_end.py

--- Python/List_1/common_end.py
+++ Python/List_1/common_end.py
@@ -2,9 +2,9 @@
 # element or they have the same last element. Both arrays will be length 1 or
 # more.
 
-# common_end([1, 2, 3], [7, 3]) -> True
-# common_end([1, 2, 3], [7, 3, 2]) -> False
-# common_end([1, 2, 3], [1, 3]) -> True
+# common_end([1, 2, 3], [7, 3]) --> True
+# common_end([1, 2, 3], [7, 3, 2]) --> False
+# common_end([1, 2, 3], [1, 3]) --> True
 
 def common_end(a, b):
   return (a[0] == b[0] or a[-1] == b[-1])
