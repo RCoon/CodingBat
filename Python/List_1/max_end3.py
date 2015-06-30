a798323ff4e3057df4d41d60c137a66b092516b5
Ryan Coon <RCoon@users.noreply.github.com>
2015-06-29 23:23:34 -0500

    Update max_end3.py

--- Python/List_1/max_end3.py
+++ Python/List_1/max_end3.py
@@ -2,9 +2,9 @@
 # first and last elements in the array, and set all the other elements to
 # be that value. Return the changed array. 
 
-# max_end3([1, 2, 3]) -> [3, 3, 3]
-# max_end3([11, 5, 9]) -> [11, 11, 11]
-# max_end3([2, 11, 3]) -> [3, 3, 3]
+# max_end3([1, 2, 3]) --> [3, 3, 3]
+# max_end3([11, 5, 9]) --> [11, 11, 11]
+# max_end3([2, 11, 3]) --> [3, 3, 3]
 
 def max_end3(nums):
   largest = max(nums[0], nums[-1])
