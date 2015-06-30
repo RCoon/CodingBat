c9e697345e1b2d10c42927f992d88689fb9cd21c
Ryan Coon <RCoon@users.noreply.github.com>
2015-06-29 23:22:05 -0500

    Update make_ends.py

--- Python/List_1/make_ends.py
+++ Python/List_1/make_ends.py
@@ -2,9 +2,9 @@
 # last elements from the original array. The original array will be length 1
 # or more. 
 
-# make_ends([1, 2, 3]) ---> [1, 3]
-# make_ends([1, 2, 3, 4]) ---> [1, 4]
-# make_ends([7, 4, 6, 2]) ---> [7, 2]
+# make_ends([1, 2, 3]) --> [1, 3]
+# make_ends([1, 2, 3, 4]) --> [1, 4]
+# make_ends([7, 4, 6, 2]) --> [7, 2]
 
 def make_ends(nums):
   return ([nums[0], nums[-1]])
