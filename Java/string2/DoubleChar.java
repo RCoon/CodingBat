3a8e050ae1b34778bd11f1de27b2eb9e84e29137
Ryan Coon <RCoon@users.noreply.github.com>
2015-07-02 18:07:56 -0500

    Update DoubleChar.java

--- Java/string2/DoubleChar.java
+++ Java/string2/DoubleChar.java
@@ -4,9 +4,9 @@ package string2;
  * Given a string, return a string where for every char in the original, 
  * there are two chars.
  * 
- * doubleChar("The") -> "TThhee"
- * doubleChar("AAbb") -> "AAAAbbbb"
- * doubleChar("Hi-There") -> "HHii--TThheerree"
+ * doubleChar("The") --> "TThhee"
+ * doubleChar("AAbb") --> "AAAAbbbb"
+ * doubleChar("Hi-There") --> "HHii--TThheerree"
  */
 
 public class DoubleChar {
