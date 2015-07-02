7d85bf6abb7e495a18019b04b28ef843320d36e0
Ryan Coon <RCoon@users.noreply.github.com>
2015-07-02 17:57:46 -0500

    Update sleep_in.py

--- Python/Warmup_1/sleep_in.py
+++ Python/Warmup_1/sleep_in.py
@@ -2,9 +2,9 @@
 # is True if we are on vacation. We sleep in if it is not a weekday or we're
 # on vacation. Return True if we sleep in.
 
-# sleep_in(False, False) → True
-# sleep_in(True, False) → False
-# sleep_in(False, True) → True
+# sleep_in(False, False) --> True
+# sleep_in(True, False) --> False
+# sleep_in(False, True) --> True
 
 def sleep_in(weekday, vacation):
   return (not weekday or vacation)
