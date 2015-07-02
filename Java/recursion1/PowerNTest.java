4fefeebf606e223bb36792bf4e5fa3ddf6c97d3a
Ryan Coon <RCoon@users.noreply.github.com>
2015-07-02 18:04:04 -0500

    Update PowerNTest.java

--- Java/recursion1/PowerNTest.java
+++ Java/recursion1/PowerNTest.java
@@ -4,9 +4,9 @@ package recursion1;
  * Given base and n that are both 1 or more, compute recursively (no loops) the
  * value of base to the n power, so powerN(3, 2) is 9 (3 squared).
  * 
- * powerN(3, 1) -> 3
- * powerN(3, 2) -> 9
- * powerN(3, 3) -> 27
+ * powerN(3, 1) --> 3
+ * powerN(3, 2) --> 9
+ * powerN(3, 3) --> 27
  */
 
 public class PowerNTest {
