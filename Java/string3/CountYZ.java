95dd3fc870cb84638d03c0ea18b72e740fd77f19
Ryan Coon <RCoon@users.noreply.github.com>
2015-07-02 18:08:11 -0500

    Update CountYZ.java

--- Java/string3/CountYZ.java
+++ Java/string3/CountYZ.java
@@ -6,9 +6,9 @@ package string3;
  * (not case sensitive). We'll say that a y or z is at the end of a word if 
  * there is not an alphabetic letter immediately following it.
  * 
- * countYZ("fez day") -> 2
- * countYZ("day fez") -> 2
- * countYZ("day fyyyz") -> 2
+ * countYZ("fez day") --> 2
+ * countYZ("day fez") --> 2
+ * countYZ("day fyyyz") --> 2
  */
 
 import java.util.*;
