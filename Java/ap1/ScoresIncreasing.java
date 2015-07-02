b26c86e723eeacaa5195b8a2d12791072658e494
Ryan Coon <RCoon@users.noreply.github.com>
2015-07-02 17:59:00 -0500

    Update ScoresIncreasing.java

--- Java/ap1/ScoresIncreasing.java
+++ Java/ap1/ScoresIncreasing.java
@@ -4,9 +4,9 @@ package ap1;
  * Given an array of scores, return true if each score is equal or greater 
  * than the one before. The array will be length 2 or more.
  * 
- * scoresIncreasing({1, 3, 4}) -> true
- * scoresIncreasing({1, 3, 2}) -> false
- * scoresIncreasing({1, 1, 4}) -> true
+ * scoresIncreasing({1, 3, 4}) --> true
+ * scoresIncreasing({1, 3, 2}) --> false
+ * scoresIncreasing({1, 1, 4}) --> true
  */
 
 public class ScoresIncreasing {
@@ -26,5 +26,4 @@ public class ScoresIncreasing {
 		  }
 		  return true;
 	}
-
 }
