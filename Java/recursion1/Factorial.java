98f79b30a089ea4ea82e94eaa0a9c784c5287204
Ryan Coon <RCoon@users.noreply.github.com>
2015-07-02 18:03:38 -0500

    Update Factorial.java

--- Java/recursion1/Factorial.java
+++ Java/recursion1/Factorial.java
@@ -4,9 +4,9 @@ package recursion1;
  * Given n of 1 or more, return the factorial of n, which is 
  * n * (n-1) * (n-2) ... 1. Compute the result recursively (without loops).
  * 
- * factorial(1) -> 1
- * factorial(2) -> 2
- * factorial(3) -> 6
+ * factorial(1) --> 1
+ * factorial(2) --> 2
+ * factorial(3) --> 6
  */
 
 public class Factorial {
