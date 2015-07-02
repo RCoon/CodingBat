b899183d2a17c483a1c916d01f1dec41b3235c87
Ryan Coon <RCoon@users.noreply.github.com>
2015-07-02 18:02:32 -0500

    Update ChangeXYTest.java

--- Java/recursion1/ChangeXYTest.java
+++ Java/recursion1/ChangeXYTest.java
@@ -4,9 +4,9 @@ package recursion1;
  * Given a string, compute recursively (no loops) a new string where all the
  * lowercase 'x' chars have been changed to 'y' chars.
  * 
- * changeXY("codex") -> "codey"
- * changeXY("xxhixx") -> "yyhiyy"
- * changeXY("xhixhix") -> "yhiyhiy"
+ * changeXY("codex") --> "codey"
+ * changeXY("xxhixx") --> "yyhiyy"
+ * changeXY("xhixhix") --> "yhiyhiy"
  */
 
 public class ChangeXYTest {
