0a1f222b53f18c5cfa0e9c01e620b1b459830893
Ryan Coon <RCoon@users.noreply.github.com>
2015-07-02 18:10:01 -0500

    Update StringTimes.java

--- Java/warmup2/StringTimes.java
+++ Java/warmup2/StringTimes.java
@@ -4,9 +4,9 @@ package warmup2;
  * Given a string and a non-negative int n, return a larger string that is n
  * copies of the original string.
  * 
- * stringTimes("Hi", 2) = "HiHi"
- * stringTimes("Hi", 3) = "HiHiHi"
- * stringTimes("Hi", 1) = "Hi"
+ * stringTimes("Hi", 2) --> "HiHi"
+ * stringTimes("Hi", 3) --> "HiHiHi"
+ * stringTimes("Hi", 1) --> "Hi"
  */
 
 public class StringTimes {
