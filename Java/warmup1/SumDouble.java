cd496850392f19e7949d3b78a2d3dd672040e750
Ryan Coon <RCoon@users.noreply.github.com>
2015-07-02 18:09:27 -0500

    Update SumDouble.java

--- Java/warmup1/SumDouble.java
+++ Java/warmup1/SumDouble.java
@@ -4,9 +4,9 @@ package warmup1;
  * Given two int values, return their sum. Unless the two values are the same,
  * then return double their sum.
  * 
- * sumDouble(1, 2) = 3
- * sumDouble(3, 2) = 5
- * sumDouble(2, 2) = 8
+ * sumDouble(1, 2) --> 3
+ * sumDouble(3, 2) --> 5
+ * sumDouble(2, 2) --> 8
  */
 public class SumDouble {
 
