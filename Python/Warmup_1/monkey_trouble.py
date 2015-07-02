15be6ea2d73dde4f0c0e5d3010129829d35b0d29
Ryan Coon <RCoon@users.noreply.github.com>
2015-07-02 17:57:29 -0500

    Update monkey_trouble.py

--- Python/Warmup_1/monkey_trouble.py
+++ Python/Warmup_1/monkey_trouble.py
@@ -2,9 +2,9 @@
 # if each is smiling. We are in trouble if they are both smiling or if neither
 # of them is smiling. Return True if we are in trouble.
 
-# monkey_trouble(True, True) → True
-# monkey_trouble(False, False) → True
-# monkey_trouble(True, False) → False
+# monkey_trouble(True, True) --> True
+# monkey_trouble(False, False) --> True
+# monkey_trouble(True, False) --> False
 
 def monkey_trouble(a_smile, b_smile):
   return ((a_smile and b_smile) or (not a_smile and not b_smile))
