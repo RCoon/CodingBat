bda812a70efcead3d75a2f0bcd4c0820a8d5c6c1
Ryan Coon <RCoon@users.noreply.github.com>
2015-07-02 18:03:01 -0500

    Update Count8Test.java

--- Java/recursion1/Count8Test.java
+++ Java/recursion1/Count8Test.java
@@ -7,9 +7,9 @@ package recursion1;
  * rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost
  * digit (126 / 10 is 12).
  * 
- * count8(8) -> 1
- * count8(818) -> 2
- * count8(8818) -> 4
+ * count8(8) --> 1
+ * count8(818) --> 2
+ * count8(8818) --> 4
  */
 
 public class Count8Test {
