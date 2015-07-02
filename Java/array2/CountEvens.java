2801a0ce2351972b1f627c7cafb68549236c67ec
Ryan Coon <RCoon@users.noreply.github.com>
2015-07-02 17:59:57 -0500

    Update CountEvens.java

--- Java/array2/CountEvens.java
+++ Java/array2/CountEvens.java
@@ -4,9 +4,9 @@ package array2;
  * Return the number of even ints in the given array. Note: the % "mod" 
  * operator computes the remainder, e.g. 5 % 2 is 1.
  * 
- * countEvens({2, 1, 2, 3, 4}) -> 3
- * countEvens({2, 2, 0}) -> 3
- * countEvens({1, 3, 5}) -> 0
+ * countEvens({2, 1, 2, 3, 4}) --> 3
+ * countEvens({2, 2, 0}) --> 3
+ * countEvens({1, 3, 5}) --> 0
  */
 
 public class CountEvens {
