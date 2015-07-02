3afe3a05637e2dd3adc3d0837d8c7750264104ca
Ryan Coon <RCoon@users.noreply.github.com>
2015-07-02 18:07:40 -0500

    Update HelloName.java

--- Java/string1/HelloName.java
+++ Java/string1/HelloName.java
@@ -4,9 +4,9 @@ package string1;
  * Given a string name, e.g. "Bob", return a greeting of the form
  * "Hello Bob!".
  * 
- * helloName("Bob") = "Hello Bob!"
- * helloName("Alice") = "Hello Alice!"
- * helloName("X") = "Hello X!"
+ * helloName("Bob") --> "Hello Bob!"
+ * helloName("Alice") --> "Hello Alice!"
+ * helloName("X") --> "Hello X!"
  */
 
 public class HelloName {
