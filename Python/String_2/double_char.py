355860a4919ee5e2b7165e50ea710068c1b01342
Ryan Coon <RCoon@users.noreply.github.com>
2015-07-02 17:57:11 -0500

    Update double_char.py

--- Python/String_2/double_char.py
+++ Python/String_2/double_char.py
@@ -1,9 +1,9 @@
 # Given a string, return a string where for every char in the original, there
 # are two chars.  
 
-# double_char('The') → 'TThhee'
-# double_char('AAbb') → 'AAAAbbbb'
-# double_char('Hi-There') → 'HHii--TThheerree'
+# double_char('The') --> 'TThhee'
+# double_char('AAbb') --> 'AAAAbbbb'
+# double_char('Hi-There') --> 'HHii--TThheerree'
 
 def double_char(str):
   result = ''
