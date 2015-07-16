bee34801a2e2fb01a9f754a5d64105eb4528f975
AmishLou <rybread354@aol.com>
2015-07-16 13:42:38 -0500

    Added new problem

---
+++ Python/String_2/cat_dog.py
@@ -0,0 +1,13 @@
+# Return True if the string "cat" and "dog" appear the same number of times
+# in the given string. 
+
+# cat_dog('catdog') --> True
+# cat_dog('catcat') --> False
+# cat_dog('1cat1cadodog') --> True
+
+def cat_dog(str):
+  return (str.count("cat") == str.count("dog"))
+  
+print(cat_dog('catdog'))
+print(cat_dog('catcat'))
+print(cat_dog('1cat1cadodog'))
