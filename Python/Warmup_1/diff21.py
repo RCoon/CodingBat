09d82fa259b861b2f2e75b623b11ae22895b238e
Ryan Coon <RCoon@users.noreply.github.com>
2015-07-26 23:50:18 -0500

    Added new problem

---
+++ Python/Warmup_1/diff21.py
@@ -0,0 +1,16 @@
+# Given an int n, return the absolute difference between n and 21, except return
+# double the absolute difference if n is over 21. 
+
+# diff21(19) --> 2
+# diff21(10) --> 11
+# diff21(21) --> 0
+
+def diff21(n):
+  if n > 21:
+    return 2 * int((math.fabs(n - 21)))  
+  else:
+    return int(math.fabs(n - 21)) 
+    
+print(diff21(19))
+print(diff21(10))
+print(diff21(0))
