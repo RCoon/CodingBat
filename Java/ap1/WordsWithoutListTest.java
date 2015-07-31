46e3fc43c32781e24d684fc044c314cad2c5df33
Ryan Coon <rybread354@aol.com>
2015-07-31 07:26:17 -0500

    Added new problems

---
+++ Java/ap1/WordsWithoutListTest.java
@@ -0,0 +1,36 @@
+package ap1;
+
+import java.util.*;
+
+/*
+ * Given an array of strings, return a new List (e.g. an ArrayList) where
+ * all the strings of the given length are omitted. See wordsWithout() below
+ * which is more difficult because it uses arrays.
+ * 
+ * wordsWithoutList({"a", "bb", "b", "ccc"}, 1) --> {"bb", "ccc"}
+ * wordsWithoutList({"a", "bb", "b", "ccc"}, 3) --> {"a", "bb", "b"}
+ * wordsWithoutList({"a", "bb", "b", "ccc"}, 4) --> {"a", "bb", "b", "ccc"}
+ */
+
+public class WordsWithoutListTest {
+    public static void main(String[] args) {
+		WordsWithoutListTest test = new WordsWithoutListTest();
+		System.out.println(test.wordsWithoutList(
+						   new String[] {"a", "bb", "b", "ccc"}, 1));
+		System.out.println(test.wordsWithoutList(
+						   new String[] {"a", "bb", "b", "ccc"}, 3));
+		System.out.println(test.wordsWithoutList(
+						   new String[] {"a", "bb", "b", "ccc"}, 4));
+	}
+	
+	public List wordsWithoutList(String[] words, int len) {
+	    ArrayList<String> newWords = new ArrayList<String>();
+	    
+	    for (int i = 0; i < words.length; i++) {
+	        if (words[i].length() != len) {
+	            newWords.add(words[i]);
+	        }
+	    }
+	    return newWords;
+	}
+}
